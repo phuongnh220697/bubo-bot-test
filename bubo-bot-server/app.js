2c9de908b3da01e8acd38854088e2249999e1d53
Phuong Hoai <phuongnh220697@gmail.com>
2024-05-25 21:33:26 +0700

    Vercel deploy

--- bubo-bot-server/app.js
+++ bubo-bot-server/app.js
@@ -11,10 +11,6 @@ var apiRouter = require('./routes/api');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 app.use(cors());
 app.use(logger('dev'));
 app.use(bodyParser.json());
